a6982a3691baf2c143b3d21074751a815dafe0cd
habiba <f219099@cfd.nu.edu.pk>
2024-08-02 14:25:00 +0500

    Update App.css and Payments.js

--- src/components/Payments.js
+++ src/components/Payments.js
@@ -1,68 +1,68 @@
 import React from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import { FaCcVisa, FaCcMastercard, FaCcPaypal, FaBitcoin, FaApplePay, FaGooglePay } from 'react-icons/fa';
+import { Container } from 'react-bootstrap';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+import 'animate.css';
+
+import AVTR1 from '../assets/img/avatar1.jpeg';
+import AVTR2 from '../assets/img/avatar2.jpeg';
+import AVTR3 from '../assets/img/avatar3.jpeg';
+import AVTR4 from '../assets/img/avatar4.jpeg';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Robert Downey',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Bruce Wayne',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Zendaya Lastname',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.'
+  }
+];
 
 export const Payments = () => {
   return (
     <Container className="payments-section">
-      <h2 className="section-title">How Can You Pay Us</h2>
-      <Row className="justify-content-center">
-        <Col md={4} className="payment-card">
-          <Card>
-            <Card.Body className="text-center">
-              <FaCcVisa className="payment-icon" />
-              <Card.Title>Visa</Card.Title>
-              <Card.Text>Fast and secure payments with your Visa card.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4} className="payment-card">
-          <Card>
-            <Card.Body className="text-center">
-              <FaCcMastercard className="payment-icon" />
-              <Card.Title>Mastercard</Card.Title>
-              <Card.Text>Use your Mastercard for quick transactions.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4} className="payment-card">
-          <Card>
-            <Card.Body className="text-center">
-              <FaCcPaypal className="payment-icon" />
-              <Card.Title>PayPal</Card.Title>
-              <Card.Text>Pay easily with your PayPal account.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4} className="payment-card">
-          <Card>
-            <Card.Body className="text-center">
-              <FaBitcoin className="payment-icon" />
-              <Card.Title>Bitcoin</Card.Title>
-              <Card.Text>Cryptocurrency payments with Bitcoin.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4} className="payment-card">
-          <Card>
-            <Card.Body className="text-center">
-              <FaApplePay className="payment-icon" />
-              <Card.Title>Apple Pay</Card.Title>
-              <Card.Text>Use Apple Pay for secure payments.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md={4} className="payment-card">
-          <Card>
-            <Card.Body className="text-center">
-              <FaGooglePay className="payment-icon" />
-              <Card.Title>Google Pay</Card.Title>
-              <Card.Text>Pay conveniently with Google Pay.</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      <h2>Testimonials</h2>
+      <Swiper
+        className='container testimonials__container'
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => (
+          <SwiperSlide key={index} className='testimonial'>
+            <div className='client__avatar'>
+              <img src={avatar} alt={`${name} Avatar`} />
+            </div>
+            <h5 className='client__name'>{name}</h5>
+            <small className='client__review'>{review}</small>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod
+        neque provident velit, rem explicabo excepturi id illo molestiae
+        blanditiis, eligendi dicta officiis asperiores delectus quasi inventore
+        debitis quo.
+      </p>
     </Container>
   );
 };
